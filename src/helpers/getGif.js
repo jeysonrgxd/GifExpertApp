903a66b80f6b055705e152a1db4df25ac913c107
jeyson <jeysonrgxd@gmail.com>
2020-12-02 12:28:27 -0500

    corregiendo http por https en url giffy

--- src/helpers/getGif.js
+++ src/helpers/getGif.js
@@ -1,5 +1,5 @@
 export const getGif = async (categoria) => {
-   const url = `http://api.giphy.com/v1/gifs/search?api_key=6SWLzp5ocE96gxC0IM5MRktqb3lMcnf6&q=${encodeURI(categoria)}&limit=10`
+   const url = `https://api.giphy.com/v1/gifs/search?api_key=6SWLzp5ocE96gxC0IM5MRktqb3lMcnf6&q=${encodeURI(categoria)}&limit=10`
 
    const resp = await fetch(url)
    const { data } = await resp.json()
